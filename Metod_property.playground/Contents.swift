e1ee336d50c79a0df67af0a5e06d517fc5e7d756
KalinchevDmitry <Dmitry@mykalinchev.ru>
2023-03-31 00:04:04 +0300

    task 1-2

--- Metod_property.playground/Contents.swift
+++ Metod_property.playground/Contents.swift
@@ -10,6 +10,9 @@ enum ListArtist: String {
     case jakson = "Mike Jakson"
     case adele = "Adele"
     case kino = "Kino"
+    case vivaldi = "Vivaldi"
+    case betkoven = "Betkhoven"
+    
 }
 
 enum Country: String {
@@ -18,6 +21,7 @@ enum Country: String {
     case russia = "Russia"
     case finland = "Finland"
     case germany = "Germany"
+    case italy = "Italy"
 }
 
 struct Track {
@@ -42,7 +46,9 @@ var track = Track(name: "Fear of the dark", artist: .iron, country: .british, ti
 var track2 = Track(name: "Halloween", artist: .halloween, country: .germany, time: 5.20, numberTrack: Track.number)
 var track3 = Track(name: "Moon", artist: .jakson, country: .usa, time: 3.32, numberTrack: Track.number)
 var track4 = Track(name: "Star name Sun", artist: .kino, country: .russia, time: 4.12, numberTrack: Track.number)
-
+var track5 = Track(name: "Summer", artist: .vivaldi, country: .italy, time: 7.12, numberTrack: Track.number)
+var track6 = Track(name: "Star", artist: .adele, country: .usa, time: 3.55, numberTrack: Track.number)
+var track7 = Track(name: "Simphoni #17", artist: .betkoven, country: .germany, time: 8.32, numberTrack: Track.number)
 
 
 class Category {
@@ -58,12 +64,16 @@ class Category {
     
     func addTrack (track:Track) {
         list.append(track)
-        print("Track \"\(track.name)\" add in category \(name). Now in category \(numbers) track.")
+        if numbers == 1 {
+            print("Track \"\(track.name)\" add in category \"\(name)\". Now in category \(numbers) track.")
+        } else { print("Track \"\(track.name)\" add in category \"\(name)\". Now in category \(numbers) tracks.") }
     }
     
     func deleteTrack (track:Track) {
         list.removeAll(where: {$0.name == track.name})
-        print("Track \"\(track.name)\" delete from category \(name). Now in category \(numbers) track.")
+        if numbers == 1 {
+            print("Track \"\(track.name)\" delete from category \"\(name)\". Now in category \(numbers) track.")
+        }    else {print("Track \"\(track.name)\" delete from category \"\(name)\". Now in category \(numbers) tracks.")}
     }
 }
 
@@ -73,10 +83,54 @@ var rock = Category(name: "Rock" )
 rock.addTrack(track: track)
 rock.addTrack(track: track2)
 rock.addTrack(track: track4)
+rock.deleteTrack(track: track4)
+
+
+// Task 2
 
 var pop = Category(name: "Pop")
 
 pop.addTrack(track: track3)
-
-rock.deleteTrack(track: track4)
 pop.deleteTrack(track: track4)
+pop.addTrack(track: track6)
+
+var classic = Category(name: "Classic")
+classic.addTrack(track: track7)
+classic.addTrack(track: track5)
+
+
+class Library {
+    var name: String
+    var numbers:Int {
+        return listCat.count
+    }
+    
+    lazy var listCat: [Category] = []
+    
+    init (name:String) {
+        self.name = name
+        
+    }
+    
+    func addLibrary(category:Category) {
+        listCat.append(category)
+        if numbers == 1 {
+            print("Category` \"\(category.name)\" add in library \"\(name)\". Now in library \(numbers) category.")
+        } else { print("Category \"\(category.name)\" add in library \"\(name)\". Now in library \(numbers) categorys.") }
+    }
+    
+    func deleteLibrary(category:Category) {
+        listCat.removeAll(where: {$0.name == category.name})
+        if numbers == 1 {
+            print("Category` \"\(category.name)\" add in library \"\(name)\". Now in library \(numbers) category.")
+        } else { print("Category \"\(category.name)\" add in library \"\(name)\". Now in library \(numbers) categorys.") }
+    }
+}
+var libraryForSoul = Library(name: "For soul" )
+libraryForSoul.addLibrary(category: classic)
+libraryForSoul.addLibrary(category: pop)
+
+var libraryForTranning = Library(name: "For tranning")
+libraryForTranning.addLibrary(category: rock)
+
+
